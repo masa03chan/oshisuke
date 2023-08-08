75a5370c903ce6d5781ee1c460dedac6f83e2a0f
masa03chan <127210098+masa03chan@users.noreply.github.com>
2023-08-08 04:06:40 +0000

    Edit top

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_content_search
+
+  def set_content_search
+    @search = Content.ransack(params[:q])
+    @contents = @search.result
+  end
+
 end
--- app/models/content.rb
+++ app/models/content.rb
@@ -1,2 +1,7 @@
 class Content < ApplicationRecord
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["title"]
+  end
+
 end
