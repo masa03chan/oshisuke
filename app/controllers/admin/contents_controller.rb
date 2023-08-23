325a1452a42cb6f36df051d47b9083d79273b955
masa03chan <127210098+masa03chan@users.noreply.github.com>
2023-08-23 14:10:31 +0000

    Finish admin_end_user_index

--- app/controllers/admin/contents_controller.rb
+++ app/controllers/admin/contents_controller.rb
@@ -1,16 +1,43 @@
 class Admin::ContentsController < ApplicationController
-  def new
-  end
+  before_action :authenticate_admin!, only: [:edit, :update]
+  before_action :set_q, only: [:index, :search]
 
-  def create
+  def search
+    @results = @q.result
   end
 
   def index
+    if params[:q]
+      @results = @q.result
+      @contents = @q.result.where(status: true).page(params[:page]).per(10)
+      @quantity = @q.result.where(status: true).count
+    else
+      @contents = Content.where(status: true).page(params[:page]).per(10)
+      @quantity = Content.count
+    end
   end
 
-  def show
+  def edit
+    @content = Content.find(params[:id])
   end
 
-  def destroy
+  def update
+    @content = Content.find(params[:id])
+    if @content.update(content_params)
+      flash[:notice] = "コンテンツ情報を編集しました。"
+      redirect_to content_path(@content.id)
+    else
+      render :edit
+    end
+  end
+
+private
+
+  def set_q
+    @q = Content.ransack(params[:q])
+  end
+
+  def content_params
+    params.require(:content).permit(:image, :title, :caption, :status, :public_link)
   end
 end
--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -1,13 +1,31 @@
 class Admin::EndUsersController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
+    @end_users = EndUser.page(params[:page]).per(10)
   end
 
   def show
+    @end_user = EndUser.find(params[:id])
   end
 
   def edit
+    @end_user = EndUser.find(params[:id])
   end
 
   def update
+    @end_user = EndUser.find(params[:id])
+    if @end_user.update(end_user_params)
+      flash[:notice] = "会員情報を編集しました。"
+      redirect_to admin_end_users_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def end_user_params
+    params.require(:end_user).permit(:name, :email, :profile_image, :caption, :encrypted_password, :is_deleted)
   end
 end
