f89cd41f581cd27be664a65db9f2422976aa901c
masa03chan <127210098+masa03chan@users.noreply.github.com>
2023-08-30 14:22:48 +0000

    deploy migration_edit_2nd

--- db/migrate/20230804055400_create_contents.rb
+++ db/migrate/20230804055400_create_contents.rb
@@ -3,7 +3,7 @@ class CreateContents < ActiveRecord::Migration[6.1]
     create_table :contents do |t|
       t.string :title, null: false
       t.boolean :status, null: false, default: true
-      t.text :caption, null: false
+      t.text :caption, null: false, limit: 2000
       t.string :public_link, null: false
       t.timestamps
     end
